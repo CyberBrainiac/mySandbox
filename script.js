73fe7f372ecff083445731d07e91384c6f61067a
Arseniy <arsenij.arxipov.1998@gmail.com>
2023-02-22 13:01:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 "use strict";
 ////////////////////////////////////////////////////////////////////////SANDBOX/////////////////////////////////////////////////////////////////////////////////////////////////////
 
-fetch('https://example.com', {
-  method: 'GET',
-  credentials: 'include',
-  mode: 'cors',
-})
-  .then((response) => {
-    console.log(response);
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });
+// fetch('https://example.com', {
+//   method: 'GET',
+//   credentials: 'include',
+//   mode: 'cors',
+// })
+//   .then((response) => {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
 
 
 
