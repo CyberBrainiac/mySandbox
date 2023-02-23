4df908b1d2b1c435e9a7b3819788f44049eb642f
Arseniy <arsenij.arxipov.1998@gmail.com>
2023-02-23 09:01:03 +0000

    test

--- script.js
+++ script.js
@@ -52,8 +52,47 @@ async function loadJson(url) {
   }
 }
 
-loadJson('no-such-user.json') // (3)
-  .catch(console.log); // Error: 404
+// loadJson('no-such-user.json').catch(console.log); // Error: 404
 
 
-let a = "commit";
+
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = 'HttpError';
+    this.response = response;
+  }
+}
+
+function loadJson(url) {
+  return fetch(url)
+    .then(response => {
+      console.log(response);
+      if (response.status == 200) {
+        return response.json();
+      } else {
+        throw new HttpError(response);
+      }
+    })
+}
+
+// Запрашивать логин, пока github не вернёт существующего пользователя.
+function demoGithubUser() {
+  let name = prompt("Введите логин?", "iliakan");
+
+  return loadJson(`https://api.github.com/users/${name}`)
+    .then(user => {
+      alert(`Полное имя: ${user.name}.`);
+      return user;
+    })
+    .catch(err => {
+      if (err instanceof HttpError && err.response.status == 404) {
+        alert("Такого пользователя не существует, пожалуйста, повторите ввод.");
+        return demoGithubUser();
+      } else {
+        throw err;
+      }
+    });
+}
+
+demoGithubUser();
