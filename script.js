9e7b5a6f528ade229117014e5e6aa22266b73cb9
Arseniy <arsenij.arxipov.1998@gmail.com>
2023-02-23 08:24:16 +0000

    1 commaand commit

--- script.js
+++ script.js
@@ -40,7 +40,20 @@ async function showAvatar() {
   return githubUser;
 }
 
-showAvatar();
+// showAvatar();
+
+async function loadJson(url) {
+  const response = await fetch(url)
+  
+  if (response.status == 200) {
+    return response.json();
+  } else {
+    throw new Error(response.status);
+  }
+}
+
+loadJson('no-such-user.json') // (3)
+  .catch(console.log); // Error: 404
 
 
 let a = "commit";
