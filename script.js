83965a1637b016ed17e6b473bf829226e08d2d1c
Arseniy <arsenij.arxipov.1998@gmail.com>
2023-02-24 13:03:25 +0000

    text

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
 
 async function getData() {
   const response = await fetch('https://example.com/data');
-  const data = await response.json;
+  const data = await response.json();
   return data;
 }
 
