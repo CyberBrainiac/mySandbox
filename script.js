7cbde114fddacf96278970306c32e1fc0c3a56b8
Arseniy <arsenij.arxipov.1998@gmail.com>
2023-03-06 12:45:22 +0200

    test

--- script.js
+++ script.js
@@ -1,46 +1,11 @@
 "use strict";
 ////////////////////////////////////////////////////////////////////////SANDBOX/////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// fetch('https://example.com', {
-//   method: 'GET',
-//   credentials: 'include',
-//   mode: 'cors',
-// })
-//   .then((response) => {
-//     console.log(response);
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//   });
-
-
-// async function getData() {
-//   try {
-//     const response = await fetch('https://coinmarketcap.com/api/');
-//     const data = await response.json();
-//   } catch (error) {
-//     console.log("Error:");
-//     console.log(error);
-//   }
-//   return data;
-// }
-
-// getData().then(
-//   (res) => console.log(res)
-// )
-
-
-alert("hello!");
-
-console.log(React);
-
-const Button = () => {
-  return <button>Click me!</button>
+function Button (props) {
+  // Возвращает DOM элемент. Например:
+  return <button type="submit">{props.label}</button>;
 }
+// Отрисовываем компонент Button в браузере
+ReactDOM.render(<Button label="Save" />, document.getElementById('root'))
 
-
-import toUpperCase from 'https://flavio-es-modules-example.glitch.me/uppercase.js'
-console.log(toUpperCase("small text"));
-
-ReactDOM.render(<Button />, document.getElementById('root'));
+// ReactDOM.render(<Button />, document.getElementById('root'));
