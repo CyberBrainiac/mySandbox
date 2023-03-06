227b8a22476ac17783411ecc7036fec6f5c537ac
Arseniy <arsenij.arxipov.1998@gmail.com>
2023-03-06 13:40:33 +0200

    react

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 "use strict";
 ////////////////////////////////////////////////////////////////////////SANDBOX/////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function Button (props) {
   // Возвращает DOM элемент. Например:
   return <button type="submit">{props.label}</button>;
 }
 // Отрисовываем компонент Button в браузере
-root.render(<Button label="Save" />);
-// ReactDOM.render(<Button label="Save" />, document.getElementById('root'))
+ReactDOM.render(<Button label="Save" />, document.getElementById('root'))
+// root.render(<Button label="Save" />);
 
 // ReactDOM.render(<Button />, document.getElementById('root'));
